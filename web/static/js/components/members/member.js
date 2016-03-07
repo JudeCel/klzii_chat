01b2f56d3e03eca2d374ca25e21eb6e33d79e036
Janis Miezitis <janjiss@gmail.com>
2016-03-07 17:08:31 +0200

    Fixed copying in webpack config

--- web/static/js/components/members/member.js
+++ web/static/js/components/members/member.js
@@ -1,20 +1,21 @@
 import React, {PropTypes}       from 'react';
+import Avatar from './avatar.js'
 
 const Member = ({member}) => {
   function onlineStatus(online) {
     let color = (online ? "green": "");
     return { color: color };
   }
-  
-  const { id, username, role, online } = member;
 
+  const { id, username, role, online, avatar_info } = member;
   return(
     <div key={ id }>
       Name: { username }
       <br />
       Role: { role }
       <br />
-      <div className=" glyphicon glyphicon-globe"style={onlineStatus(online)} />
+      <div className=" glyphicon glyphicon-globe" style={onlineStatus(online)} />
+      <Avatar avatar_info={avatar_info} />
     </div>
   )
 }
