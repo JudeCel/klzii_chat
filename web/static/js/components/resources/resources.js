f4c3664b67790cdc1aaa445609b9a66e6d27ef66
DainisL <dainis186@gmail.com>
2016-03-09 15:55:59 +0200

    added image UI

--- web/static/js/components/resources/resources.js
+++ web/static/js/components/resources/resources.js
@@ -1,32 +1,42 @@
 import React, {PropTypes}  from 'react';
 import { connect }         from 'react-redux';
-import { Modal }           from "react-bootstrap"
-import VideoUpload         from "./videoUpload"
-
+import { Modal }           from "react-bootstrap";
+import Constants            from '../../constants';
+import ImageUpload         from "./imageUpload";
+import VideoUpload         from "./videoUpload";
 
 const Resources =  React.createClass({
-  getInitialState() {
-    return {
-      openModalVideo: false
-    };
+  changeModalWindow(e){
+    let modal = e.target.getAttribute('data-modal');
+    this.props.dispatch({type: Constants.OPEN_RESOURCE_MODAL, modal });
   },
-  openModalVideo(){
-    this.setState({ openModalVideo: true });
-
-  },
-  closeModalVIdeo(){
-     this.setState({ openModalVideo: false });
+  closeModalWindow(){
+    this.props.dispatch({type: Constants.CLOASE_RESOURCE_MODAL });
   },
   render() {
     return (
-      <div className="col-md-12">
+      <div className="resources col-md-12">
         <div
-          onClick={this.openModalVideo}
-          className="glyphicon glyphicon-facetime-video">
-            <VideoUpload show={this.state.openModalVideo} onHide={this.closeModalVIdeo} />
+          onClick={this.changeModalWindow}
+          data-modal="image"
+          className="resource glyphicon glyphicon-picture">
+            <ImageUpload show={this.props.modalWindow =="image"} onHide={this.closeModalWindow} />
+        </div>
+        <div
+          onClick={this.changeModalWindow}
+          data-modal="video"
+          className="resource glyphicon glyphicon-film">
+            <VideoUpload show={this.props.modalWindow == "video"} onHide={this.closeModalWindow} />
         </div>
       </div>
     );
   }
 })
 export default Resources;
+
+const mapStateToProps = (state) => {
+  return {
+    modalWindow: state.resources.modalWindow
+  }
+};
+export default connect(mapStateToProps)(Resources);
--- web/static/js/reducers/resources.js
+++ web/static/js/reducers/resources.js
@@ -4,7 +4,8 @@ const initialState = {
   videos: [],
   images: [],
   audio: [],
-  fetch: false
+  fetch: false,
+  modalWindow: "" // "image", "video" "audio"
 };
 
 export default function reducer(state = initialState, action = {}) {
@@ -17,6 +18,11 @@ export default function reducer(state = initialState, action = {}) {
       return { ...state, fetch: false, images: action.images};
     case Constants.SET_AUDIO_RESOURCES:
       return { ...state, fetch: false, audio: action.audio};
+
+    case Constants.OPEN_RESOURCE_MODAL:
+      return { ...state, modalWindow: action.modal };
+    case Constants.CLOASE_RESOURCE_MODAL:
+      return { ...state, modalWindow: "" };
     default:
       return state;
   }
