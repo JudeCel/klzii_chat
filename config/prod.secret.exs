251bf2b7765ce222d2563339ca80af68caec8486
Janis Miezitis <janjiss@gmail.com>
2016-03-18 14:03:38 +0200

    Fixed secrets

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -1,25 +1,25 @@
-use Mix.Config
-
 # In this file, we keep production configuration that
 # you likely want to automate and keep it away from
 # your version control system.
 config :klzii_chat, KlziiChat.Endpoint,
-  secret_key_base: "276977DDB3E2D46257CFF10D113A704A65B490F1855619637C0A0423F9C8D884"
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Configure your database
 config :klzii_chat, KlziiChat.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  database: "kliiko_development",
-  hostname: "localhost",
+  username: System.get_env("DB_USERNAME"),
+  database: System.get_env("DB_DATABASE"),
+  hostname: System.get_env("DB_HOST"),
+  password: System.get_env("DB_PASSWORD"),
   pool_size: 10
 
 config :ex_aws,
-  access_key_id: "AKIAJV2UHLF355DYBP6A",
-  secret_access_key: "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",
-  region: "eu-west-1"
+  access_key_id: System.get_env("AWS_ACCESS_KEY_ID"),
+  secret_access_key: System.get_env("AWS_secret_access_key"),
+  region: System.get_env("AWS_region"),
+  
 config :arc,
-  bucket: "klzii-development"
+  bucket: System.get_env("ARC_REGION")
 
 config :guardian, Guardian,
-  secret_key: "SttPra/cddsnX+Vko2i8KA=="
+  secret_key: System.get_env("GUARDIAN_SECRET_KEY")
