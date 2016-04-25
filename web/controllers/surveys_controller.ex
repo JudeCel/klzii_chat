f4eceedb4f00ca07881fc81478ae5fa693ea7edf
DainisL <dainis186@gmail.com>
2016-04-25 14:20:14 +0300

    tidy up

--- web/controllers/surveys_controller.ex
+++ web/controllers/surveys_controller.ex
@@ -1,25 +1,16 @@
 defmodule KlziiChat.SurveysController do
   use KlziiChat.Web, :controller
   alias KlziiChat.{Survey, Repo, SurveyView}
-  import Ecto
+  import Ecto.Query, only: [from: 2]
 
   def show(conn, %{"id" => id}) do
-    survey =
-      Repo.get(Survey, id)
-      case Repo.get(Survey, id) do
-        nil ->
-          json(conn, %{status: :error, reason: "not found"})
-        survey ->
-          survey = Repo.preload(survey, [:resource, survey_questions: [:resource]])
-          json(conn, %{survey: SurveyView.render("survey.json", %{survey: survey}) })
-      end
-
-
-    # case ResourceService.find(account_user.id, id ) do
-    #   {:ok, resource} ->
-    #     json(conn, %{resource: ResourceView.render("resource.json", %{resource: resource}) })
-    #   {:error, reason} ->
-    #     json(conn, %{status: :error, reason: reason})
-    # end
+    from(s in Survey, preload: [:resource, survey_questions: [:resource]])
+    |> Repo.get(id)
+    |> case do
+      nil ->
+        json(conn, %{status: :error, reason: "not found"})
+      survey ->
+        json(conn, %{survey: SurveyView.render("survey.json", %{survey: survey}) })
+    end
   end
 end
