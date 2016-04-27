a5351c334ebd191f914ec076c72f5dd532b2f02e
Janis Miezitis <janjiss@gmail.com>
2016-04-27 13:22:02 +0300

    Updated CORS config

--- lib/klzii_chat/corsica/router.ex
+++ lib/klzii_chat/corsica/router.ex
@@ -2,7 +2,12 @@ defmodule KlziiChat.Corsica.Router do
   use Corsica.Router,
     allow_headers: ~w(accept cache-control pragma authorization content-type)
 
-  resource "/uploads/*", origins: [Regex.compile("^https?://(.*\.?)focus\.com" || System.get_env("CORS_URL"))]
-  resource "/api/*", origins: [Regex.compile("^https?://(.*\.?)focus\.com" || System.get_env("CORS_URL"))]
+  resource "/uploads/*", origins: [get_url_regexp]
+  resource "/api/*", origins: [get_url_regexp]
   resource "/*", origins: "*"
+
+  def get_url_regexp do
+    {:ok, regexp} = Regex.compile(System.get_env("CORS_URL") || "^https?://(.*\.?)focus\.com" )
+    regexp
+  end
 end
