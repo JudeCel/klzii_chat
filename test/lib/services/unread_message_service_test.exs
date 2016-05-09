720a59e636cd420ca3a5be206ffd9d732b7a9111
DainisL <dainis186@gmail.com>
2016-05-09 12:45:05 +0300

    fixed typo

--- test/lib/services/unread_message_service_test.exs
+++ test/lib/services/unread_message_service_test.exs
@@ -6,16 +6,16 @@ defmodule KlziiChat.Services.UnreadMessageServiceTest do
 
     list = [
       %{"id" => 3, "topic" => %{1 => %{"normal" => 2}}},
-      %{"id" => 3, "topic" => %{1 => %{"replay" => 5}}},
+      %{"id" => 3, "topic" => %{1 => %{"reply" => 5}}},
       %{"id" => 3, "topic" => %{2 => %{"normal" => 1}}},
-      %{"id" => 3, "topic" => %{2 => %{"replay" => 2}}},
+      %{"id" => 3, "topic" => %{2 => %{"reply" => 2}}},
       %{"id" => 1, "topic" => %{1 => %{"normal" => 7}}},
     ]
     resp = UnreadMessageService.group_by_topics_and_scope(list)
 
     result = %{
-      "3" => %{"topics" =>  %{ "1" => %{"normal" => 2, "replay" => 5 },
-                               "2" => %{"normal" => 1, "replay" => 2 } }
+      "3" => %{"topics" =>  %{ "1" => %{"normal" => 2, "reply" => 5 },
+                               "2" => %{"normal" => 1, "reply" => 2 } }
               },
       "1" => %{"topics" =>  %{ "1" => %{"normal" => 7 } } }
     }
@@ -24,17 +24,17 @@ defmodule KlziiChat.Services.UnreadMessageServiceTest do
 
   test "add summary to map by message scope" do
     map = %{
-      "3" => %{"topics" =>  %{ "1" => %{"normal" => 2, "replay" => 5 },
-                               "2" => %{"normal" => 1, "replay" => 2 } }
+      "3" => %{"topics" =>  %{ "1" => %{"normal" => 2, "reply" => 5 },
+                               "2" => %{"normal" => 1, "reply" => 2 } }
               },
       "1" => %{"topics" =>  %{ "1" => %{"normal" => 7 } } }
     }
     result = %{
-      "3" => %{"topics" =>  %{ "1" => %{"normal" => 2, "replay" => 5 },
-                               "2" => %{"normal" => 1, "replay" => 2 } },
-              "summary" => %{"normal" => 3, "replay" => 7 }},
+      "3" => %{"topics" =>  %{ "1" => %{"normal" => 2, "reply" => 5 },
+                               "2" => %{"normal" => 1, "reply" => 2 } },
+              "summary" => %{"normal" => 3, "reply" => 7 }},
       "1" => %{"topics" =>  %{ "1" => %{"normal" => 7 }},
-              "summary" => %{"normal" => 7, "replay" => 0 }}
+              "summary" => %{"normal" => 7, "reply" => 0 }}
     }
     resp = UnreadMessageService.calculate_summary(map)
     assert(resp == result)
