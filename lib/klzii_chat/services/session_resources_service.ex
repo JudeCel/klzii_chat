6e56546134a3c8c60c295fc9362b3f0220e5e394
yuriibodarev <yurii.bodarev@diatomenterprises.com>
2016-05-11 08:24:27 +0300

    WIP: SessionReourcesService.toggle & unit test

--- lib/klzii_chat/services/session_resources_service.ex
+++ lib/klzii_chat/services/session_resources_service.ex
@@ -5,6 +5,20 @@ defmodule KlziiChat.Services.SessionResourcesService do
   import Ecto.Query
 
   def toggle(sessionId, resourcesIds, sessionMemberId) do
+    # remove all unseleted resources
+    from(sr in SessionResource, where: not sr.resourceId in ^resourcesIds and sr.sessionId == ^sessionId)
+    |> Repo.delete_all()
 
+    # id's of the resources left
+    srIds =
+      from(sr in SessionResource, where: sr.sessionId == ^sessionId, select: sr.resourceId)
+      |> Repo.all()
+
+    # add all new resoures that are unique
+    Stream.filter(resourcesIds, &(not &1 in srIds))
+    |> Enum.each(&Repo.insert(%SessionResource{resourceId: &1, sessionId: sessionId}))
+
+    #newSR = Enum.map(resourcesIds, &(%{resourceId: &1, sessionId: sessionId}))
+    #Repo.insert_all("SessionResources", newSR)
   end
 end
--- test/lib/services/session_resoures_service_test.exs
+++ test/lib/services/session_resoures_service_test.exs
@@ -1,20 +1,27 @@
 defmodule KlziiChat.Services.SessionResourcesServiceTest do
   use KlziiChat.{ModelCase, SessionMemberCase}
   alias KlziiChat.Services.SessionResourcesService
+  alias KlziiChat.SessionResource
 
   setup %{session: session, member: member, account_user: account_user} do
-    resource = Ecto.build_assoc(
+    image_resource = Ecto.build_assoc(
       account_user.account, :resources,
       accountUserId: account_user.id,
-      name: "TEST_IMG",
+      name: "cool image",
       type: "image",
       scope: "collage"
     ) |> Repo.insert!
 
-    {:ok, session_id: session.id, member_id: member.id, resource_id: resource.id}
+    {:ok, session_id: session.id, member_id: member.id, resource_id: image_resource.id}
   end
 
   test "toggle", %{session_id: session_id, member_id: member_id, resource_id: resource_id} do
     SessionResourcesService.toggle(session_id, [resource_id], member_id)
+
+    res =
+      from(sr in SessionResource, where: sr.sessionId == ^session_id, select: sr.resourceId)
+      |> Repo.all()
+
+    assert(res = [resource_id])
   end
 end
