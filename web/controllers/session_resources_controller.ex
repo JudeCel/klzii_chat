33108405da1d164e28a05d3e1ab2fd3534832c1f
DainisL <dainis186@gmail.com>
2016-05-12 17:20:12 +0300

    fixed session resource index error

--- web/controllers/session_resources_controller.ex
+++ web/controllers/session_resources_controller.ex
@@ -8,7 +8,7 @@ defmodule KlziiChat.SessionResourcesController do
   plug :if_current_member
 
   def index(conn, _, member, _) do
-    case SessionResourcesService.get_session_resources(member.session_member.sessionId, member.session_member.id) do
+    case SessionResourcesService.get_session_resources(member.session_member.id) do
       {:ok, session_resources} ->
         json(conn, Phoenix.View.render_many(session_resources, SessionResourcesView, "show.json", as: :session_resource))
       {:error, reason} ->
