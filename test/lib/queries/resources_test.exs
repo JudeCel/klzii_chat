b28685ed65327d65fa0de2a9c021573c12b7f053
yuriibodarev <yurii.bodarev@diatomenterprises.com>
2016-05-16 10:34:48 +0300

    KlziiChat.Queries.Resources added test for exclude_by_session_id

--- test/lib/queries/resources_test.exs
+++ test/lib/queries/resources_test.exs
@@ -2,8 +2,9 @@ defmodule KlziiChat.Services.Resourecs do
   use KlziiChat.{ModelCase, SessionMemberCase}
   alias KlziiChat.Queries.Resources, as: QueriesResources
   alias KlziiChat.{Repo, Resource}
+  alias KlziiChat.Services.SessionResourcesService
 
-  setup %{account_user: account_user} do
+  setup %{account_user: account_user, session: session, member: member} do
     Ecto.build_assoc(
       account_user.account, :resources,
       accountUserId: account_user.id,
@@ -13,7 +14,7 @@ defmodule KlziiChat.Services.Resourecs do
     ) |> Repo.insert!
 
     base_query = Ecto.Query.from(r in Resource)
-    {:ok, base_query: base_query, account_user: account_user}
+    {:ok, base_query: base_query, account_user: account_user, session_id: session.id, member_id: member.id,}
   end
 
   test "when admin can see all resources", %{account_user: account_user} do
@@ -62,8 +63,42 @@ defmodule KlziiChat.Services.Resourecs do
     assert(count == 1 )
   end
 
-  test "find by params when both type and scope one not macth", %{base_query: base_query} do
+  test "find by params when both type and scope one not match", %{base_query: base_query} do
     count = QueriesResources.find_by_params(base_query, %{"scope" => ["pdf"], "type" => "file"}) |> Repo.all |> Enum.count
     assert(count == 0 )
   end
+
+  test "find by params and exclude 1 of 3 session resources by sesion id (2 left)",
+    %{account_user: account_user, member_id: member_id} do
+      resource =
+        Ecto.build_assoc(
+          account_user.account, :resources,
+          accountUserId: account_user.id,
+          name: "cool 2",
+          type: "file",
+          scope: "zip"
+        ) |> Repo.insert!
+
+      Ecto.build_assoc(
+        account_user.account, :resources,
+        accountUserId: account_user.id,
+        name: "cool 3",
+        type: "file",
+        scope: "zip"
+      ) |> Repo.insert!
+
+      %KlziiChat.AccountUser{:"AccountId" => accountId} = account_user
+
+      {:ok, _} = SessionResourcesService.add_session_resources([resource.id], member_id)
+
+      count =
+        QueriesResources.add_role_scope(account_user)
+        |> QueriesResources.find_by_params(%{})
+        |> QueriesResources.exclude_by_session_id(accountId, member_id)
+        |> Repo.all
+        |> Enum.count
+
+      assert(count == 2)
+  end
+
 end
