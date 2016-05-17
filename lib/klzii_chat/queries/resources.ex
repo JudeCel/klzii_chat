871e80e9d7a94db95eee3c1a2d9a6cb9167876d9
DainisL <dainis186@gmail.com>
2016-05-17 10:41:11 +0300

    add missing spec for KlziiChat.Queries.Resources functions

--- lib/klzii_chat/queries/resources.ex
+++ lib/klzii_chat/queries/resources.ex
@@ -2,17 +2,19 @@ defmodule KlziiChat.Queries.Resources do
   import Ecto
   import Ecto.Query
 
-  alias KlziiChat.{Resource, AccountUser}
-  alias KlziiChat.Services.SessionResourcesService
+  alias KlziiChat.{AccountUser}
 
+  @spec base_query(%AccountUser{}) :: Ecto.Query.t
   def base_query(account_user) do
     from(r in assoc(account_user.account, :resources))
   end
 
+  @spec find_by_params(Ecto.Query.t, map) :: Ecto.Query.t
   def find_by_params(base_query, params) do
     build_type(base_query, params) |> build_scope(params)
   end
 
+  @spec build_type(Ecto.Query.t, map) :: Ecto.Query.t
   def build_type(query, %{"type" => type}) when is_list(type) do
     from r in query, where: r.type in ^type
   end
@@ -23,6 +25,7 @@ defmodule KlziiChat.Queries.Resources do
     query
   end
 
+  @spec build_scope(Ecto.Query.t, map) :: Ecto.Query.t
   def build_scope(query, %{"scope" => scope}) when is_list(scope) do
     from r in query, where: r.scope in ^scope
   end
@@ -33,6 +36,7 @@ defmodule KlziiChat.Queries.Resources do
     query
   end
 
+  @spec exclude_by_ids(Ecto.Query.t, [%KlziiChat.SessionResource{}]) :: Ecto.Query.t
   def exclude_by_ids(query, session_resources) do
     session_resource_ids = Enum.map(session_resources, fn(%{resourceId: resource_id}) -> resource_id end)
     from(r in query, where: not r.id in ^session_resource_ids)
