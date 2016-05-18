7b05684999e64b5dc0c45d407f850183aee163f9
yuriibodarev <yurii.bodarev@diatomenterprises.com>
2016-05-18 13:23:27 +0300

    Reporting service test setup

--- test/lib/services/reporting_service_test.exs
+++ test/lib/services/reporting_service_test.exs
@@ -1,14 +1,17 @@
 defmodule KlziiChat.Services.ReportingServiceTest do
-  use KlziiChat.SessionMemberCase
+  use KlziiChat.{ModelCase, SessionMemberCase}
   alias KlziiChat.Services.MessageService
 
-  setup %{session: session, session_topic_1: session_topic_1, member: member, member2: member2} do
-    {:ok, session_id: session.id, session_topic_id: session_topic_1.id, member_id: member.id, member2_id: member2.id}
+  setup %{session_topic_1: session_topic_1, member: member} do
+    # insert messages
+
+
+    {:ok, session_topic_id: session_topic_1.id, member_id: member.id}
   end
 
   test "convert topic history to CSV", %{member_id: member_id, session_topic_id: session_topic_id} do
     MessageService.history(session_topic_id, member_id)
-    |> IO.inspect
+    |> IO.inspect()
   end
 
 end
