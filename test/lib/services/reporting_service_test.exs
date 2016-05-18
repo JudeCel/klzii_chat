8a398bc9324ed7c44392510854b9129e19d3eb14
yuriibodarev <yurii.bodarev@diatomenterprises.com>
2016-05-18 16:03:14 +0300

    Convert Messages History to CSV

--- test/lib/services/reporting_service_test.exs
+++ test/lib/services/reporting_service_test.exs
@@ -1,6 +1,7 @@
 defmodule KlziiChat.Services.ReportingServiceTest do
   use KlziiChat.{ModelCase, SessionMemberCase}
   alias KlziiChat.Services.MessageService
+  alias Ecto.DateTime
 
   setup %{session_topic_1: session_topic_1, member: member} do
     # insert messages
@@ -25,8 +26,30 @@ defmodule KlziiChat.Services.ReportingServiceTest do
   end
 
   test "convert topic history to CSV", %{member: member, session_topic_id: session_topic_id} do
-    MessageService.history(session_topic_id, member)
+    {:ok, topic_history} = MessageService.history(session_topic_id, member)
+
+    topic_history
+    |> Stream.map(&filter(&1))
+    |> CSV.Encoder.encode(headers: true)
+    |> Enum.to_list()
     |> IO.inspect()
+
   end
+
+  def filter(%{body: body, emotion: emotion, replyId: replyId,
+    session_member: %{username: name}, star: star, time: time}) do
+
+    %{"name" => name, "comment" => body, "date" => DateTime.to_string(time),
+        "is tagged" => to_string(star), "emotion" => emotion}
+  end
+
+#  %{body: "test message 2", emotion: 2, has_voted: false, id: 105,
+#   permissions: %{can_delete: true, can_edit: true, can_new_message: true,
+#     can_reply: true, can_star: true, can_vote: true}, replies: [],
+#   replyId: nil,
+#   session_member: %{avatarData: %{"base" => 0, "body" => 0, "desk" => 0,
+#       "face" => 3, "hair" => 0, "head" => 0}, colour: "00000", id: 507,
+#     role: "facilitator", username: "cool member"}, star: false,
+#   time: #Ecto.DateTime<2016-05-18 12:47:04>, votes_count: 0}
 
 end
