df1753cb12f439960dce327441c46eccc774d0da
yuriibodarev <yurii.bodarev@diatomenterprises.com>
2016-05-18 14:28:30 +0300

    Reporting service test

--- test/lib/services/reporting_service_test.exs
+++ test/lib/services/reporting_service_test.exs
@@ -5,12 +5,27 @@ defmodule KlziiChat.Services.ReportingServiceTest do
   setup %{session_topic_1: session_topic_1, member: member} do
     # insert messages
 
+    Ecto.build_assoc(
+      session_topic_1, :messages,
+      sessionTopicId: session_topic_1.id,
+      sessionMemberId: member.id,
+      body: "test message 1",
+      emotion: 1
+    ) |> Repo.insert!()
 
-    {:ok, session_topic_id: session_topic_1.id, member_id: member.id}
+    Ecto.build_assoc(
+      session_topic_1, :messages,
+      sessionTopicId: session_topic_1.id,
+      sessionMemberId: member.id,
+      body: "test message 2",
+      emotion: 2
+    ) |> Repo.insert!()
+
+    {:ok, session_topic_id: session_topic_1.id, member: member}
   end
 
-  test "convert topic history to CSV", %{member_id: member_id, session_topic_id: session_topic_id} do
-    MessageService.history(session_topic_id, member_id)
+  test "convert topic history to CSV", %{member: member, session_topic_id: session_topic_id} do
+    MessageService.history(session_topic_id, member)
     |> IO.inspect()
   end
 
