dc7d608c02eee3f10d591c35efe470c1902f0491
yuriibodarev <yurii.bodarev@diatomenterprises.com>
2016-05-24 21:43:06 +0300

    JS for avatar processing added to html.eex template

--- lib/klzii_chat/services/reporting_service.ex
+++ lib/klzii_chat/services/reporting_service.ex
@@ -53,15 +53,4 @@ defmodule KlziiChat.Services.ReportingService do
     ])
   end
 
-  #  %{body: "test message 2", emotion: 2, has_voted: false, id: 105,
-  #   permissions: %{can_delete: true, can_edit: true, can_new_message: true,
-  #     can_reply: true, can_star: true, can_vote: true}, replies: [],
-  #   replyId: nil,
-  #   session_member: %{avatarData: %{"base" => 0, "body" => 0, "desk" => 0,
-  #       "face" => 3, "hair" => 0, "head" => 0}, colour: "00000", id: 507,
-  #     role: "facilitator", username: "cool member"}, star: false,
-  #   time: #Ecto.DateTime<2016-05-18 12:47:04>, votes_count: 0}
-
-
-
 end
