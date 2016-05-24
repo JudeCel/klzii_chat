5c283be0d27c584d77dff6d5157e9db2a2ea0aab
yuriibodarev <yurii.bodarev@diatomenterprises.com>
2016-05-25 00:05:13 +0300

    avatars for HTML/PDF reports

--- web/static/js/reporting/avatar.js
+++ web/static/js/reporting/avatar.js
@@ -1,4 +1,9 @@
-function setAvatar(sessionMember) {
-  var s = Snap('.svg' + sessionMember.id);
-  var bigCircle = s.circle(50, 50, 25);
+function setAvatar(sessionMember, imgPath) {
+  var avatar = Snap('.svg' + sessionMember.id);
+  avatar.image(imgPath + '/base_00.svg', 0, 0, 152, 140);
+  avatar.image(imgPath + '/face_00.svg', 0, 0, 152, 140);
+  avatar.image(imgPath + '/body_00.svg', 0, 0, 152, 140);
+  avatar.image(imgPath + '/hair_00.svg', 0, 0, 152, 140);
+  avatar.image(imgPath + '/desk_00.svg', 0, 0, 152, 140);
+  avatar.image(imgPath + '/head_00.svg', 0, 0, 152, 140);
 }
