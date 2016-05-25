0210039f0feb220898596f5265fe941840ca4dc3
yuriibodarev <yurii.bodarev@diatomenterprises.com>
2016-05-25 10:06:45 +0300

    avatars for HTML/PDF reports

--- web/static/js/reporting/avatar.js
+++ web/static/js/reporting/avatar.js
@@ -1,9 +1,15 @@
 function setAvatar(sessionMember, imgPath) {
   var avatar = Snap('.svg' + sessionMember.id);
-  avatar.image(imgPath + '/base_00.svg', 0, 0, 152, 140);
-  avatar.image(imgPath + '/face_00.svg', 0, 0, 152, 140);
-  avatar.image(imgPath + '/body_00.svg', 0, 0, 152, 140);
-  avatar.image(imgPath + '/hair_00.svg', 0, 0, 152, 140);
-  avatar.image(imgPath + '/desk_00.svg', 0, 0, 152, 140);
-  avatar.image(imgPath + '/head_00.svg', 0, 0, 152, 140);
+  ad = sessionMember.avatarData;
+  avatar.image(imgPath + '/base_' + padToTwo(ad.base) + '.svg', 0, 0, 76, 70);
+  avatar.image(imgPath + '/face_' + padToTwo(ad.face) + '.svg', 0, 0, 76, 70);
+  avatar.image(imgPath + '/body_' + padToTwo(ad.body) + '.svg', 0, 0, 76, 70);
+  avatar.image(imgPath + '/hair_' + padToTwo(ad.hair) + '.svg', 0, 0, 76, 70);
+  avatar.image(imgPath + '/desk_' + padToTwo(ad.desk) + '.svg', 0, 0, 76, 70);
+  avatar.image(imgPath + '/head_' + padToTwo(ad.head) + '.svg', 0, 0, 76, 70);
+}
+
+function padToTwo(number) {
+  if (number <= 99) number = '0' + number;
+  return number;
 }
