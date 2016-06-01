0abb7434de3b933f7e01942c47dd5c9ae6fc0883
DainisL <dainis186@gmail.com>
2016-06-01 11:23:01 +0300

    added test for SocketHelper

---
+++ test/lib/helpers/socket_helper_test.exs
@@ -0,0 +1,10 @@
+defmodule KlziiChat.Helpers.SocketHelperTest do
+  use ExUnit.Case, async: true
+  alias KlziiChat.Helpers.SocketHelper
+
+  test "get_session_member" do
+    session_member =  %{session_id: 1}
+    socket =   Phoenix.Socket.assign(Phoenix.Socket.__struct__, :session_member, %{session_id: 1})
+    assert(SocketHelper.get_session_member(socket) == session_member)
+  end
+end
