fc0c97df080ccb4f4c84423db61df5653131a535
Alexander Semionov <alexander.semionov@diatomenterprises.com>
2016-06-06 15:37:34 +0300

    move functions to util file

--- web/static/js/components/whiteboard/actionHistory.js
+++ web/static/js/components/whiteboard/actionHistory.js
@@ -1,44 +1,14 @@
+import whiteboardUtilities  from './whiteboardUtilities';
 let undoHistory = [];
 let currentIdx = 0;
 
-function copyObject(obj) {
-  return JSON.parse(JSON.stringify(obj));
-}
-
-function areObjectsEqual(a, b) {
-  if (!a || !b) {
-    return false;
-  }
-  let x = JSON.parse(JSON.stringify(a));
-  let y = JSON.parse(JSON.stringify(b));
-  let p;
-  for(p in y) {
-    if(typeof(x[p])=='undefined') {return false;}
-  }
-
-  for(p in y) {
-    if (y[p]) {
-      switch(typeof(y[p])) {
-        case 'object':
-          if (!areObjectsEqual(y[p], x[p])) { return false; } break;
-        default:
-          if (y[p] != x[p]) { return false; }
-      }
-    } else {
-      if (x[p])
-        return false;
-    }
-  }
-  return true;
-}
-
 function addStepToUndoHistory(json) {
-  if (!areObjectsEqual(json, currentStepObject())) {
+  if (!whiteboardUtilities.areObjectsEqual(json, currentStepObject())) {
     //if made a few undo steps, then delete next redo steps first
     if (currentIdx > 0 && currentIdx < undoHistory.length - 1) {
       undoHistory = undoHistory.slice(0, currentIdx + 1);
     }
-    undoHistory.push(copyObject(json));
+    undoHistory.push(whiteboardUtilities.copyObject(json));
     currentIdx = undoHistory.length - 1;
   }
 }
