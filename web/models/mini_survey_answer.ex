6246adc61b9bbdb164632e712f24e7d9ad8d2829
DainisL <dainis186@gmail.com>
2016-06-06 09:41:28 +0300

    Update docs and hex. Added mini survey models

---
+++ web/models/mini_survey_answer.ex
@@ -0,0 +1,24 @@
+defmodule KlziiChat.MiniSurveyAnswer do
+  use KlziiChat.Web, :model
+
+  schema "MiniSurveyAnswers" do
+    belongs_to :session_member, KlziiChat.SessionMember, [foreign_key: :sessionMemberId]
+    belongs_to :mini_survey, KlziiChat.MiniSurve, [foreign_key: :miniSurveyId]
+    field :answer, :map
+    timestamps [inserted_at: :createdAt, updated_at: :updatedAt]
+  end
+  
+  @required_fields ~w(messageId sessionMemberId)
+  @optional_fields ~w()
+
+  @doc """
+  Creates a changeset based on the `model` and `params`.
+
+  If no params are provided, an invalid changeset is returned
+  with no validation performed.
+  """
+  def changeset(model, params \\ %{}) do
+    model
+    |> cast(params, (@required_fields ++  @optional_fields))
+  end
+end
