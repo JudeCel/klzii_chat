670ee58d8ec0eb87b4868d3d2674be2e07c57a2f
GuskiS <edvards@lazdans.lv>
2016-06-10 10:58:49 +0300

    Added notification listener, added sample report actions to frontend

---
+++ web/static/js/actions/reports.js
@@ -0,0 +1,56 @@
+import Constants from '../constants';
+
+function _errorData(data) {
+  return {
+    type: Constants.SHOW_NOTIFICATION,
+    data: {
+      type: 'error',
+      message: data.error
+    }
+  };
+}
+
+const Actions = {
+  subscribeReportsEvents:(channel) => {
+    return dispatch => {
+      channel.on('report_updated', (data) => {
+        return dispatch({ type: Constants.UPDATE_REPORT, data: data });
+      });
+    }
+  },
+  index:(channel, callback) => {
+    return dispatch => {
+      channel.push('get_session_reports')
+      .receive('ok', (data) => {
+        dispatch({ type: Constants.SET_REPORTS, data: data.reports });
+        if(callback) callback();
+      }).receive('error', (data) => {
+        dispatch(_errorData(data));
+      });
+    }
+  },
+  create:(channel, params, callback) => {
+    return dispatch => {
+      channel.push('create_session_topic_report', params)
+      .receive('ok', (data) => {
+        dispatch({ type: Constants.CREATE_REPORT, data: data });
+        if(callback) callback();
+      }).receive('error', (data) => {
+        dispatch(_errorData(data));
+      });
+    }
+  },
+  delete:(channel, reportId, callback) => {
+    return dispatch => {
+      channel.push('delete_session_topic_report', { id: reportId })
+      .receive('ok', (data) => {
+        dispatch({ type: Constants.DELETE_REPORT, data: data });
+        if(callback) callback();
+      }).receive('error', (data) => {
+        dispatch(_errorData(data));
+      });
+    }
+  }
+}
+
+export default Actions;
