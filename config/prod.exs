7fc05c841f114341031b9be5cdecffaa1754a632
DainisL <dainis186@gmail.com>
2016-06-11 16:00:39 +0300

    try fix prod port issue

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :klzii_chat, KlziiChat.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: System.get_env("SERVER_HOST") || "localhost", port: 80],
+  url: [host:  {:system, "SERVER_HOST"}, port: {:system, "PORT"}],
   cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production
