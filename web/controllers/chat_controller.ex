48804b3ba5540f2a17335fb2d62d67810d475679
DainisL <dainis186@gmail.com>
2016-06-15 12:04:25 +0300

    set chat token cookie

--- web/controllers/chat_controller.ex
+++ web/controllers/chat_controller.ex
@@ -8,7 +8,7 @@ defmodule KlziiChat.ChatController do
   """
   def index(conn, %{"token_dev" => token}) do
     case Mix.env do
-      env when env in [:dev, :test, :prod] ->
+      env when env in [:dev, :test] ->
         session_member = Repo.get_by!(KlziiChat.SessionMember, token: token)
         Guardian.Plug.sign_in(conn, session_member)
         |> render("index.html")
@@ -19,17 +19,23 @@ defmodule KlziiChat.ChatController do
   end
 
   def index(conn, %{"token" => token}) do
-    if chat_token = get_session(conn, :chat_token) do
+    case get_member_from_token(token) do
+      {:ok , member} ->
+        conn = Guardian.Plug.sign_in(conn, member.session_member)
+        put_resp_cookie(conn, "chat_token", Guardian.Plug.current_token(conn))
+        |> render("index.html")
+      {:error, _reason } ->
+        send_resp(conn, 401, Poison.encode!(%{error: "unauthorized"}))
+        |> halt
+    end
+  end
+
+  def index(conn, _) do
+    if chat_token = conn.cookies["chat_token"] do
       render conn, "index.html" , token: chat_token
     else
-      case get_member_from_token(token) do
-        {:ok , member} ->
-          Guardian.Plug.sign_in(conn, member.session_member)
-          |> render("index.html")
-        {:error, _reason } ->
-          send_resp(conn, 401, Poison.encode!(%{error: "unauthorized"}))
-          |> halt
-      end
+      send_resp(conn, 401, Poison.encode!(%{error: "unauthorized"}))
+      |> halt
     end
   end
 end
