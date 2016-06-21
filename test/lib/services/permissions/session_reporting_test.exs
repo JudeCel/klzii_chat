75fabe98f6094a12bd0132c87ca689a55a35dd75
yuriibodarev <yuriibodarev@gmail.com>
2016-06-21 15:35:48 +0300

    subscription_permissions check added to Permissions.SessionReporting

--- test/lib/services/permissions/session_reporting_test.exs
+++ test/lib/services/permissions/session_reporting_test.exs
@@ -1,36 +1,34 @@
 defmodule KlziiChat.Services.Permissions.SessionReportingTest do
-  use ExUnit.Case, async: true
+  use KlziiChat.{ModelCase, SessionMemberCase}
   alias KlziiChat.Services.Permissions.SessionReporting, as: SessionReportingPermissions
 
-  test "facilitator can create session reports" do
-    facilitator = %{id: 1, role: "facilitator"}
+  setup %{facilitator: facilitator, participant: participant} do
+    {:ok, facilitator: facilitator, participant: participant}
+  end
+
+  test "facilitator can create session reports", %{facilitator: facilitator} do
     assert(SessionReportingPermissions.can_create_report(facilitator))
   end
 
-  test "participant can't create session reports" do
-    participant = %{id: 2, role: "participant"}
+  test "participant can't create session reports", %{participant: participant} do
     refute(SessionReportingPermissions.can_create_report(participant))
   end
 
 
-  test "facilitator can delete session reports" do
-    facilitator = %{id: 1, role: "facilitator"}
+  test "facilitator can delete session reports", %{facilitator: facilitator} do
     assert(SessionReportingPermissions.can_delete_report(facilitator))
   end
 
-  test "participant can't delete session reports" do
-    participant = %{id: 2, role: "participant"}
+  test "participant can't delete session reports", %{participant: participant} do
     refute(SessionReportingPermissions.can_delete_report(participant))
   end
 
 
-  test "facilitator can get session reports" do
-    facilitator = %{id: 1, role: "facilitator"}
+  test "facilitator can get session reports", %{facilitator: facilitator} do
     assert(SessionReportingPermissions.can_get_reports(facilitator))
   end
 
-  test "participant can't get session reports" do
-    participant = %{id: 2, role: "participant"}
+  test "participant can't get session reports", %{participant: participant} do
     refute(SessionReportingPermissions.can_get_reports(participant))
   end
 
