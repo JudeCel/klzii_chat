56529ab7564b70b8a905bd2a1d1f779f768005c4
DainisL <dainis186@gmail.com>
2016-07-05 16:54:44 +0300

    fixed Gettext error

--- web/controllers/pinboard_resource_controller.ex
+++ web/controllers/pinboard_resource_controller.ex
@@ -14,7 +14,7 @@ defmodule KlziiChat.PinboardResourceController do
       {:ok, resource} ->
         case PinboardResourceService.add(member.session_member.id, session_topic_id, resource.id) do
           {:ok, pinboard_resource} ->
-              preloaded_resourve = Repo.preload(pinboard_resource, [:session_member, :resource])
+            preloaded_resourve = Repo.preload(pinboard_resource, [:session_member, :resource])
             Endpoint.broadcast!("session_topic:#{member.session_member.sessionId}", "new_pinboard_resource", preloaded_resourve)
             json(conn, %{status: :ok})
           {:error, reason} ->
