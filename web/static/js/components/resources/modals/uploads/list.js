f125e58763033c689b348a599c4c01894a6b3ba3
GuskiS <edvards@lazdans.lv>
2016-07-06 16:20:55 +0300

    Fixed resource modal window

--- web/static/js/components/resources/modals/uploads/list.js
+++ web/static/js/components/resources/modals/uploads/list.js
@@ -2,21 +2,31 @@ import React, {PropTypes} from 'react';
 import { connect }        from 'react-redux';
 import UploadListItem     from './listItem';
 import UploadTypes        from './types/index';
+import mixins             from '../../../../mixins';
+import Actions            from '../../../../actions/session_resource';
 
 const UploadList = React.createClass({
+  mixins: [mixins.helpers],
   chooseResourceType(resources) {
     return this.props[resources];
   },
+  componentDidUpdate(props, state) {
+    const { dispatch, currentUserJwt, modalData } = this.props;
+
+    if(props.modalData.type != modalData.type) {
+      dispatch(Actions.index(currentUserJwt, { type: this.get_session_resource_types(modalData.type) }));
+    }
+  },
   render() {
-    const { modalName } = this.props;
-    const sessionResources = this.chooseResourceType(modalName);
+    const { modalData } = this.props;
+    const sessionResources = this.chooseResourceType(modalData.type);
 
     return (
       <ul className='list-group'>
-        <UploadListItem key={ 'none' } justInput={ true } modalName={ modalName } />
+        <UploadListItem key={ 'none' } justInput={ true } modalName={ modalData.type } />
         {
           sessionResources.map((sr) =>
-            <UploadListItem key={ sr.id } sessionResourceId={sr.id} resource={ sr.resource } modalName={ modalName } />
+            <UploadListItem key={ modalData.type + sr.id } sessionResourceId={sr.id} resource={ sr.resource } modalName={ modalData.type } />
           )
         }
       </ul>
@@ -26,6 +36,8 @@ const UploadList = React.createClass({
 
 const mapStateToProps = (state) => {
   return {
+    modalData: state.modalWindows.currentModalData,
+    currentUserJwt: state.members.currentUser.jwt,
     image: state.resources.images,
     video: state.resources.videos,
     audio: state.resources.audios
