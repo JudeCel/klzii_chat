31fd91ea7f1173bb37fde4d15e2a97ca7f9e9e59
DainisL <dainis186@gmail.com>
2016-07-07 12:12:55 +0300

    use Permissions.ErrorsHelper for Permissions.PinboardResource

--- lib/klzii_chat/services/permissions/pinboard_resource.ex
+++ lib/klzii_chat/services/permissions/pinboard_resource.ex
@@ -1,5 +1,6 @@
 defmodule KlziiChat.Services.Permissions.PinboardResource do
   import KlziiChat.Services.Permissions.Validations
+  import KlziiChat.Services.Permissions.ErrorsHelper, only: [formate_error: 1]
 
   @spec error_messages :: Map.t
   def error_messages do
@@ -12,24 +13,20 @@ defmodule KlziiChat.Services.Permissions.PinboardResource do
   def can_enable(member) do
     roles = ~w(facilitator)
     has_role(member.role, roles)
-    |> add_response
+    |> formate_error
   end
 
   @spec can_add_resource(Map.t) :: {:ok} | {:error, String.t}
   def can_add_resource(member) do
     roles = ~w(participant)
     has_role(member.role, roles)
-    |> add_response
+    |> formate_error
   end
 
   @spec can_remove_resource(Map.t, Map.t) :: {:ok} | {:error, String.t}
   def can_remove_resource(member, object) do
     role = ~w(facilitator)
     (has_owner(member, object, :sessionMemberId) || has_role(member.role, role))
-    |> add_response
-  end
-
-  defp add_response(status) do
-    if(status, do: {:ok}, else: {:error, error_messages.action_not_allowed})
+    |> formate_error
   end
 end
