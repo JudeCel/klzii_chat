f6db952279cab1bf0e6a0e065f0b2701f6a85900
DainisL <dainis186@gmail.com>
2016-08-12 16:14:34 +0300

    mix deps.update

--- mix.exs
+++ mix.exs
@@ -41,7 +41,6 @@ defmodule KlziiChat.Mixfile do
      {:poison, "~> 2.2"},
      {:arc_ecto, "~> 0.4.2"},
      {:arc, "~> 0.5.3"},
-     {:plug, "~> 1.2"},
      {:ex_aws, "~> 0.5"},
      {:httpotion, "~> 3.0"},
      {:postgrex, "~> 0.11.2"},
