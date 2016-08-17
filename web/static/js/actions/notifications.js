67f839e356918f06af558e0c687b894feece38d3
DainisL <dainis186@gmail.com>
2016-08-17 14:12:22 +0300

    fixed DE625

--- web/static/js/actions/notifications.js
+++ web/static/js/actions/notifications.js
@@ -9,18 +9,16 @@ const Actions = {
     }
   },
   showErrorNotification(dispatch, errors) {
-
+    let messages = [], errorObject = errors.errors || errors;
 
     if (errors instanceof Error) {
-
+      messages.push(handleStandartErrorObject(errors));
+    }else{
+      let keys = Object.keys(errorObject);
+      keys.map((key, index) => {
+        messages.push(_handleErrorType(key, errorObject[key]));
+      });
     }
-
-    let messages = [], errorObject = errors.errors || errors;
-    let keys = Object.keys(errorObject);
-
-    keys.map((key, index) => {
-      messages.push(_handleErrorType(key, errorObject[key]));
-    });
 
     messages.map((error, index) => {
       dispatch({ type: Constants.SHOW_NOTIFICATION, data: error });
@@ -44,6 +42,13 @@ var titles = {
   format: 'Format error',
   system: 'System error'
 };
+function handleStandartErrorObject(error) {
+  if (error.status == 413) {
+    return _handleErrorType("system", "File is too big");
+  }else {
+    return _handleErrorType("unhandled", error.message);
+  }
+}
 
 function _parentElement(children) {
   return `<ul>${children}</ul>`;
