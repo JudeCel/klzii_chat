5cfd07b0cea2f87bffc05d157f16b92c58d300a3
GuskiS <edvards@lazdans.lv>
2016-09-11 21:15:41 +0300

    Improved text button design

--- web/static/js/components/whiteboard_new/toolbar/text.js
+++ web/static/js/components/whiteboard_new/toolbar/text.js
@@ -1,32 +1,41 @@
 import React       from 'react';
-import { connect } from 'react-redux';
-import mixins      from '../../../mixins';
+import { OverlayTrigger, Button, Popover } from 'react-bootstrap';
 
 const ButtonsText = React.createClass({
-  mixins: [mixins.modalWindows],
   getInitialState() {
     return { buttonType: 'text' };
   },
-  onSelect(text) {
-    this.closeAllModals();
-    this.props.setText(this.state.buttonType, text);
+  onClick(action) {
+    if(action == 'remove') {
+      this.refs.text.hide();
+    }
+    else if(this.refs.input.value.length) {
+      this.refs.text.hide();
+      this.props.setText(this.state.buttonType, this.refs.input.value);
+    }
   },
-  onClick() {
-    this.openSpecificModal('whiteboardText', { select: this.onSelect });
+  popoverInput() {
+    return (
+      <Popover id='wb-buttons-text' className='input-group'>
+        <input ref='input' type='text' className='form-control no-border-radius' />
+        <span onClick={ this.onClick.bind(this, 'accept') } className='input-group-addon no-border-radius cursor-pointer'>
+          <i className='glyphicon glyphicon-ok' aria-hidden='true' />
+        </span>
+        <span onClick={ this.onClick.bind(this, 'remove') } className='input-group-addon no-border-radius cursor-pointer'>
+          <i className='glyphicon glyphicon-remove' aria-hidden='true' />
+        </span>
+      </Popover>
+    )
   },
   render() {
     return (
-      <button className={ this.props.getClassnameParent(this.state.buttonType) + 'btn btn-default' } onClick={ this.onClick }>
-        <i className='fa fa-font' aria-hidden='true' />
-      </button>
+      <OverlayTrigger ref='text' trigger='click' placement='top' rootClose overlay={ this.popoverInput() }>
+        <Button className={ this.props.getClassnameParent(this.state.buttonType) + 'btn btn-default' }>
+          <i className='fa fa-font' aria-hidden='true' />
+        </Button>
+      </OverlayTrigger>
     )
   }
 });
 
-const mapStateToProps = (state) => {
-  return {
-    modalWindows: state.modalWindows
-  }
-};
-
-export default connect(mapStateToProps)(ButtonsText);
+export default ButtonsText;
