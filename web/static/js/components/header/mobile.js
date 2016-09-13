bb0e8d9a2d55f6a63095cd9288b8850ff790a6a6
Nazar Perepichka <nazar.perepichka@diatomenterprises.com>
2016-09-13 09:41:36 +0300

    TA1247 - 95% done

--- web/static/js/components/header/mobile.js
+++ web/static/js/components/header/mobile.js
@@ -1,9 +1,11 @@
 import React, {PropTypes} from 'react';
 import { connect }        from 'react-redux';
+import Badge              from '../sessionTopics/badge';
+import Actions            from '../../actions/sessionTopic';
 
 const MobileHeader = React.createClass({
   getInitialState() {
-    return { mobileSideMenuVisibility: false };
+    return { mobileSideMenuVisibility: false, mobileSideMenuTopicsVisibility: false };
   },
   clickIfExists(className) {
     var el = document.getElementsByClassName(className)[0];
@@ -38,14 +40,39 @@ const MobileHeader = React.createClass({
     };
   },
   toggleMenu() {
+    this.setState({mobileSideMenuTopicsVisibility: false});
     this.setState({mobileSideMenuVisibility: !this.state.mobileSideMenuVisibility});
+  },
+  toggleTopics() {
+    this.setState({mobileSideMenuTopicsVisibility: !this.state.mobileSideMenuTopicsVisibility});
   },
   getMobileSideMenuStyle() {
     return { 
       display: (this.state.mobileSideMenuVisibility ? "block" : "none")
     };
   },
+  getMobileSideMenuTopicsStyle() {
+    return { 
+      display: (this.state.mobileSideMenuTopicsVisibility ? "block" : "none")
+    };
+  },
+  changeSessionTopic(id) {
+    const { dispatch, channel, whiteboardChannel } = this.props;
+    dispatch(Actions.changeSessionTopic(channel, whiteboardChannel, id));
+    this.toggleMenu();
+  },
+  directMessageBadge() {
+    const { unreadDirectMessages } = this.props;
+    let count = 0;
+    for(var i in unreadDirectMessages) {
+    count += unreadDirectMessages[i];
+    }
+    return (
+    <span className={ 'badge badge-messages-' + count  }>{ count }</span>
+    );
+  },
   render() {
+      const { sessionTopics, unread_messages } = this.props;
 
     return (
       <div className='header-innerbox header-innerbox-mobile'>
@@ -63,14 +90,34 @@ const MobileHeader = React.createClass({
           <div className='mobile-side-menu-content'>
             <ul>
               <li className='navbar-title'>Talk Radio</li>
-              <li>Morning Story</li>
-              <li onClick={this.messagesClick} style={ this.getMenuItemStyle("icon-message") }>Messages</li>
-              <li onClick={this.helpClick} style={ this.getMenuItemStyle("icon-help") }>Help</li>
-              <li onClick={this.logoutClick} style={ this.getMenuItemStyle("log-out") }>Log out</li>
+              <li onClick={this.toggleTopics}><span className='fa fa-coffee'></span>Morning Story <span className='fa fa-angle-right'></span></li>
+              <li onClick={this.messagesClick} style={ this.getMenuItemStyle("icon-message") }><span className='fa fa-comment'></span>Messages { this.directMessageBadge() }</li>
+              <li onClick={this.helpClick} style={ this.getMenuItemStyle("icon-help") }><span className='fa fa-question'></span>Help</li>
+              <li onClick={this.logoutClick} style={ this.getMenuItemStyle("log-out") }><span className='fa fa-sign-out'></span>Log out</li>
             </ul>
             <div className='powered-by'>
               Powered by <a href='http://www.klzii.com'>klsii</a>
             </div>
+          </div>
+          <div className='mobile-side-menu-topics' style={ this.getMobileSideMenuTopicsStyle() }>
+            <ul>
+              <li className='navbar-back'>
+                <span className='fa icon-reply' onClick={ this.toggleTopics }></span>
+              </li>
+                {
+                  sessionTopics.map((sessionTopic) => {
+                    return (
+                      <li key={ 'sessionTopic-' + sessionTopic.id } className='clearfix' onClick={ this.changeSessionTopic.bind(this, sessionTopic.id) }>
+                        <span className='pull-left'>{ sessionTopic.name }</span>
+                        <span className='pull-right'>
+                          <Badge type='reply' data={ unread_messages.session_topics[sessionTopic.id] } />
+                          <Badge type='normal' data={ unread_messages.session_topics[sessionTopic.id] } />
+                        </span>
+                      </li>
+                    )
+                  })
+                }
+            </ul>
           </div>
         </div>
       </div>
@@ -78,4 +125,15 @@ const MobileHeader = React.createClass({
   }
 });
 
-export default MobileHeader;
+const mapStateToProps = (state) => {
+  return {
+    unread_messages: state.messages.unreadMessages,
+    session: state.chat.session,
+    channel: state.sessionTopic.channel,
+    current: state.sessionTopic.current,
+    sessionTopics: state.sessionTopic.all,
+    whiteboardChannel: state.whiteboard.channel
+  };
+};
+
+export default connect(mapStateToProps)(MobileHeader);
