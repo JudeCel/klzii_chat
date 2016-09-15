dcadb5ed3092bf6104d49560356a87eabe72b969
GuskiS <edvards@lazdans.lv>
2016-09-15 13:54:04 +0300

    Saving progress

--- web/views/reporting/preview_view.ex
+++ web/views/reporting/preview_view.ex
@@ -4,5 +4,16 @@ defmodule KlziiChat.Reporting.PreviewView do
   def get_emotion_url(emotion) do
     "/images/emotions_static/#{emotion}_sprite_01.jpg"
   end
-  
+
+  def media_image_position(message) do
+    "emotion-chat-section push-image " <> if(length([ message.replies ]) > 0, do: "media-top", else:  "media-bottom")
+  end
+
+  def body_classname(message) do
+    "body-section col-md-12 #{message.session_member.role}"
+  end
+
+  def emotion_chat_class(message) do
+    "emotion-chat-#{message.emotion}"
+  end
 end
