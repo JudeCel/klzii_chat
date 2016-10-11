7ee90fe1d8472793f65c94fab195c93a8dff94bb
Nazar Perepichka <nazar.perepichka@diatomenterprises.com>
2016-10-11 14:26:56 +0300

    tests fixes

--- test/helpers/pagination_test.exs
+++ test/helpers/pagination_test.exs
@@ -5,20 +5,22 @@ defmodule KlziiChat.PaginationTest do
   @items_on_page 2
   @page 2
   @all_data [1, 2, 3, 4, 5]
+  @data_second_page [3, 4]
+  @pages_result 3
 
   test "pages test" do
     pages = PagesHelper.get_pages(@all_data, @items_on_page)
-    assert(pages == 3)
+    assert(pages == @pages_result)
   end
 
   test "date data test" do
     data = PagesHelper.get_page_data(@all_data, @page, @items_on_page)
-    assert([3, 4] = data)
+    assert(@data_second_page == data)
     assert(length(data) == @items_on_page)
   end
 
   test "pagination test" do
-    assert({[3, 4], 3} = PagesHelper.paginate(@all_data, @page, @items_on_page))
+    assert({@data_second_page, @pages_result} == PagesHelper.paginate(@all_data, @page, @items_on_page))
   end
 
 end
