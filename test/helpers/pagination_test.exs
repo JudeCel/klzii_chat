2834b0edfc4d547561429e7b72b1c1446b7082cd
Nazar Perepichka <nazar.perepichka@diatomenterprises.com>
2016-10-11 14:22:49 +0300

    PR fixes

--- test/helpers/pagination_test.exs
+++ test/helpers/pagination_test.exs
@@ -2,14 +2,23 @@ defmodule KlziiChat.PaginationTest do
   use KlziiChat.ModelCase, async: true
   alias KlziiChat.Helpers.{PagesHelper}
 
-  test "pagination test" do
-    items_on_page = 2
-    page = 2
-    all_data = [1, 2, 3, 4, 5]
-    {data, pages} = PagesHelper.paginate(all_data, page, items_on_page)
+  @items_on_page 2
+  @page 2
+  @all_data [1, 2, 3, 4, 5]
 
-    assert length(data) == items_on_page
-    assert pages == 3
+  test "pages test" do
+    pages = PagesHelper.get_pages(@all_data, @items_on_page)
+    assert(pages == 3)
+  end
+
+  test "date data test" do
+    data = PagesHelper.get_page_data(@all_data, @page, @items_on_page)
+    assert([3, 4] = data)
+    assert(length(data) == @items_on_page)
+  end
+
+  test "pagination test" do
+    assert({[3, 4], 3} = PagesHelper.paginate(@all_data, @page, @items_on_page))
   end
 
 end
