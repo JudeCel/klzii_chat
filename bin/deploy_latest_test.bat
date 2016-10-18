01e917a6c7f7d6de5fa657a079460aab0c0fcf1c
Nazar Perepichka <nazar.perepichka@diatomenterprises.com>
2016-10-18 10:12:20 +0300

    misspell fix

--- bin/deploy_latest_test.bat
+++ bin/deploy_latest_test.bat
@@ -1,4 +1,4 @@
-echo "Deploying latest test klzii_admin image from docker hub"
+echo "Deploying latest test klzii_chat image from docker hub"
 call gcloud container clusters get-credentials klzii-test
 call kubectl delete deployment chat
 call kubectl apply -f ../deploy/test/chat.yml
