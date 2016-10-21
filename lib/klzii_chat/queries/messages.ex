6717aebabc6b7175010ea2544a45b61d9d52ec70
DainisL <dainis186@gmail.com>
2016-10-21 12:45:59 +0300

    DE923

--- lib/klzii_chat/queries/messages.ex
+++ lib/klzii_chat/queries/messages.ex
@@ -2,11 +2,13 @@ defmodule KlziiChat.Queries.Messages do
   alias KlziiChat.{Message, SessionMember}
   import Ecto.Query, only: [from: 2]
 
+    # this is a hotfix for DE923 should be replaced by TA1330
   @spec base_query(integer) :: Ecto.Query
   def base_query(session_topic_id) do
     from message in Message,
     where: message.sessionTopicId == ^session_topic_id,
     where: is_nil(message.replyId),
+    where: [replayLevel: 0],
     order_by: [asc: :createdAt]
   end
 
@@ -35,11 +37,14 @@ defmodule KlziiChat.Queries.Messages do
   end
   def exclude_by_role(query, _, true), do: query
 
+
+  # this is a hotfix for DE923 should be replaced by TA1330
   @spec join_replies(Ecto.Query) :: Ecto.Query
   def join_replies(query) do
-    replies_query = from(st in Message, order_by: [asc: :createdAt], preload: [:session_member, :votes, :replies])
+    replies_query = from(st in Message, where: [replayLevel: 2], order_by: [asc: :createdAt], preload: [:session_member, :votes, :replies])
+    replies_nested_query = from(st in Message, where: [replayLevel: 1], order_by: [asc: :createdAt], preload: [:session_member, :votes, replies: ^replies_query])
     from message in query,
-    preload: [replies: ^replies_query]
+    preload: [replies: ^replies_nested_query]
   end
 
   @spec join_votes(Ecto.Query) :: Ecto.Query
