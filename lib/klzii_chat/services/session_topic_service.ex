28dc401579c8794926895d60e561fecdf75e75b4
Nazar Perepichka <nazar.perepichka@diatomenterprises.com>
2016-11-30 14:35:32 +0200

    DE1084

--- lib/klzii_chat/services/session_topic_service.ex
+++ lib/klzii_chat/services/session_topic_service.ex
@@ -11,7 +11,7 @@ defmodule KlziiChat.Services.SessionTopicService do
   @spec board_message(Integer, Integer, Map) :: {:ok, Map } | {:error, String.t}
   def board_message(session_member_id, session_topic_id, %{"message" => message}) do
     session_member = Repo.get!(SessionMember, session_member_id)
-    session_topic = KlziiChat.Queries.SessionTopic.find(session_topic_id)
+    session_topic = Repo.get!(SessionTopic, session_topic_id)
 
     case SessionTopicPermissions.can_board_message(session_member) do
       {:ok} ->
