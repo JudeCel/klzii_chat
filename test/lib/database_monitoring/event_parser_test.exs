b4c98ca23141e848995aa8d7d8c4c509e57d6fa2
DainisL <dainis186@gmail.com>
2016-12-12 14:28:35 +0200

    update database monitoring event parser
    change event structure use datakey for row

--- test/lib/database_monitoring/event_parser_test.exs
+++ test/lib/database_monitoring/event_parser_test.exs
@@ -4,25 +4,31 @@ defmodule KlziiChat.DatabaseMonitoring.EventParserTest do
 
   @id 3
   @session_id 3
-  @payloade "{\"table\" : \"SessionTopics\", \"id\" : #{@id}, \"session_id\" : #{@session_id}, \"type\" : \"UPDATE\"}"
+  @payloade "{\"table\" : \"SessionTopics\", \"data\" : {\"id\": #{@id}, \"name\": \"Cool Session Topic1\", \"sessionId\":  #{@session_id} }, \"type\" : \"DELETE\"}"
 
+  @unhandle_event EventParser.messages.errors.unhandle_event
   describe "succses"  do
     test "processe_event" do
       assert({:ok, "Running in Test ENV"} = EventParser.processe_event("table_update", @payloade))
     end
 
     test "decode_message" do
-      assert(%{"id" => @id} = EventParser.decode_message("table_update", @payloade))
+      assert(%{"data" => %{"id" => @id}} = EventParser.decode_message("table_update", @payloade))
     end
 
-    test "when event select_job" do
-      map = %{"table" =>  "SessionTopics", "id" =>  @session_id, "session_id" => @session_id}
-      assert({:ok, EventParser, :session_topics, [@session_id]} = EventParser.select_job(map))
+    test "when event select_job  for SessionTopics" do
+      map = %{"table" =>  "SessionTopics", "data" =>  %{"id" => @id, "sessionId" => @session_id }}
+      assert({:ok, :session_topics, [@session_id]} = EventParser.select_job(map))
+    end
+
+    test "when event select_job  for Invites" do
+      invite_id = 2
+      map = %{"table" =>  "Invites", "data" =>  %{"id" => invite_id}}
+      assert({:ok, :invites, [^invite_id]} = EventParser.select_job(map))
     end
 
     test "when unhandle event select_job" do
-      message = EventParser.messages.errors.unhandle_event
-      assert({:error, ^message} = EventParser.select_job(%{}))
+      assert({:error, @unhandle_event} = EventParser.select_job(%{}))
     end
   end
 end
