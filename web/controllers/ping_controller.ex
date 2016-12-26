6afef0334c37aa1368d6046e7d4dadfb8ddf1b3d
DainisL <dainis186@gmail.com>
2016-12-26 14:58:56 +0200

    added inspect for info

--- web/controllers/ping_controller.ex
+++ web/controllers/ping_controller.ex
@@ -6,8 +6,9 @@ defmodule KlziiChat.PingController do
       put_status(200) |>
       text("")
   end
-  
+
   def info(conn, params) do
+    IO.inspect(params)
     send_resp(conn, 200, Poison.encode!(params))
   end
 end
