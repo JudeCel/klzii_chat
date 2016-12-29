5b3c776add5fcb40cf23f0fd494bb377d73d24c1
Nazar Perepichka <nazar.perepichka@diatomenterprises.com>
2016-12-29 14:13:06 +0200

    TA1465 init commit

--- lib/klzii_chat/services/permissions/direct_message.ex
+++ lib/klzii_chat/services/permissions/direct_message.ex
@@ -5,17 +5,19 @@ defmodule KlziiChat.Services.Permissions.DirectMessage do
   @spec can_write(Map.t, Map.t) :: {:ok } | {:error, String.t}
   def can_write(current_member, other_member) do
     participant = ["participant"]
+    observer = ["observer"]
     facilitator = ["facilitator"]
 
     current_member.id != other_member.id && (
       has_role(current_member.role, participant) && has_role(other_member.role, facilitator) ||
-      has_role(current_member.role, facilitator) && has_role(other_member.role, participant)
+      has_role(current_member.role, facilitator) && has_role(other_member.role, participant) ||
+      has_role(current_member.role, observer) && has_role(other_member.role, facilitator) 
     ) |> formate_error
   end
 
   @spec can_direct_message(Map.t) :: {:ok } | {:error, String.t}
   def can_direct_message(member) do
-    roles = ["facilitator", "participant"]
+    roles = ["facilitator", "participant", "observer"]
     has_role(member.role, roles)
     |> formate_error
   end
