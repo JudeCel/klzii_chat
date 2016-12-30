f0a2bb9dab6685b0910735418c71daf207b539df
Nazar Perepichka <nazar.perepichka@diatomenterprises.com>
2016-12-30 13:47:01 +0200

    ready

--- web/static/js/components/modals/observerList.js
+++ web/static/js/components/modals/observerList.js
@@ -17,17 +17,20 @@ const ObserverListModal = React.createClass({
 
     if (onlineObservers.length) {
       return onlineObservers.map((observer) =>
-        <div className='col-md-3' key={ observer.id }>
+        <div className='col-md-3 cursor-pointer' key={ observer.id } onClick={ this.writeToObserver.bind(this, observer) }>
           <div className='icon-eye'></div>
           <div className='observer-name'>
             <div style={{ backgroundColor: observer.colour }}>{ observer.username }</div>
           </div>
         </div>
       )
-    }
-    else {
+    } else {
       return <div className='text-center'>No Spectators at this moment</div>
     }
+  },
+  writeToObserver(observer) {
+    this.closeAllModals();
+    this.openSpecificModal('directMessage', { member: observer });
   },
   render() {
     let show = this.showSpecificModal('observerList');
