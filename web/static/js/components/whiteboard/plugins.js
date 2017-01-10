a6a4161422bb309ef5487067dc39fc56ac1d7203
Alexander Semionov <alexander.semionov@diatomenterprises.com>
2017-01-10 13:58:21 +0200

    minor svg text changes

--- web/static/js/components/whiteboard/plugins.js
+++ web/static/js/components/whiteboard/plugins.js
@@ -1,11 +1,9 @@
 SVG.Element.prototype.draw.extend({
   'text': {
-    init:function(e){
+    init: function(e){
       this.startPoint.x = e.clientX;
       this.startPoint.y = e.clientY;
-      var point = this.startPoint;
-
-      this.el.attr({ x: point.x, y: point.y, 'font-size': 24 });
+      this.el.attr({ x: this.startPoint.x, y: this.startPoint.y, 'font-size': 24 });
     },
     calc: function (e) {
       var text = {
@@ -17,17 +15,15 @@ SVG.Element.prototype.draw.extend({
       text.height = point.y - text.y;
       this.snapToGrid(1);
 
-      // When width is less than one, we have to draw to the left
-      // which means we have to move the start-point to the left
+      //When no width/height is present - means that the element hasn't appeared on screen yet
       if(text.width < 1) {
-        text.x = text.x + text.width/2;
-        text.width = -text.width;
+        text.x = text.x - this.el.node.clientWidth/2;
+        text.width = this.el.node.clientWidth;
       }
 
       // ...same with height
       if (text.height < 1) {
-        text.y = text.y + text.height;
-        text.height = -text.height;
+        text.height = this.el.node.clientHeight;
       }
       this.el.attr(text);
     }
