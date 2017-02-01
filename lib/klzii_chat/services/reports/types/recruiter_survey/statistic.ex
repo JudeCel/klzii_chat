628f24daa49a84a4c0dcd336bc65d7ebc30df6a0
DainisL <dainis186@gmail.com>
2017-02-01 18:51:18 +0200

    update statistic calculation

--- lib/klzii_chat/services/reports/types/recruiter_survey/statistic.ex
+++ lib/klzii_chat/services/reports/types/recruiter_survey/statistic.ex
@@ -1,4 +1,5 @@
 defmodule KlziiChat.Services.Reports.Types.RecruiterSurvey.Statistic do
+  @countable_contact_list_fields  ["age", "gender"]
 
   @spec map_answers(List.t) :: Map.t
   def map_answers(list) do
@@ -6,6 +7,7 @@ defmodule KlziiChat.Services.Reports.Types.RecruiterSurvey.Statistic do
       Map.keys(sub_list)
       |> Enum.reduce(acc, fn(item, sub_list_acc) ->
         answer = Map.get(sub_list, item)
+
         acc_answers =
           Map.get(sub_list_acc, item, answer_default(answer))
           |> processed_answer(answer)
@@ -15,31 +17,48 @@ defmodule KlziiChat.Services.Reports.Types.RecruiterSurvey.Statistic do
     end)
   end
 
-  @spec answer_default(Map.t) :: Map.t | List.t
-  def answer_default(%{"type" => "string"}), do: %{count: 0, type: "string", values: []}
-  def answer_default(%{"type" => "number"}), do: %{count: 0, type: "number", values: %{}}
-  def answer_default(%{"type" => "object"}), do: %{count: 0, type: "object", values: %{}}
-  def answer_default(_), do: nil
+  @spec answer_default(Map.t) :: Map.t
+  def answer_default(%{"type" => "string"}), do: %{count: 0, type: "string"}
+  def answer_default(%{"type" => "number"}), do: %{count: 0, type: "number"}
+  def answer_default(%{"type" => "object"}), do: %{count: 0, type: "object"}
+  def answer_default(_), do: %{count: 0}
 
-
-
+  @spec processed_answer(Map.t, Map.t) :: Map.t
   def processed_answer(buffer, %{"type" => "number", "value" => value_nr}) do
-    values = buffer.values
+    values = Map.get(buffer, :values, %{})
     buffer_value = Map.get(values, value_nr, 0)
     update_values = Map.put(values, value_nr, buffer_value + 1)
     Map.put(buffer, :count, buffer.count + 1)
     |> Map.put(:values, update_values)
   end
   def processed_answer(buffer, %{"type" => "string", "value" => value}) do
-    values = buffer.values
+    values = Map.get(buffer, :values, [])
     Map.put(buffer, :values, (values ++ [value]))
-    |> Map.put(:count, buffer.count + 1)
+    |> update_answer_count()
   end
   def processed_answer(buffer, %{"type" => "object", "contactDetails" => contactDetails}) do
-    IO.inspect contactDetails
-    buffer
+    values = Map.get(buffer, :values, %{})
+
+    update_values =
+      @countable_contact_list_fields
+      |> Enum.reduce(values, fn(key, acc) ->
+        contactDetailsValue = Map.get(contactDetails, key)
+        buffer_value = Map.get(acc, key, %{})
+        field_value = Map.get(buffer_value, contactDetailsValue, 0)
+        new_val = Map.put(buffer_value, contactDetailsValue, field_value + 1)
+        Map.put(acc, key, new_val)
+
+      end)
+    Map.put(buffer, :type, "object")
+    |> Map.put(:values, update_values)
+    |> update_answer_count()
   end
   def processed_answer(buffer, _) do
-    buffer
+    update_answer_count(buffer)
+  end
+
+  @spec update_answer_count(Map.t) :: Map.t
+  def update_answer_count(buffer) do
+    Map.put(buffer, :count, buffer.count + 1)
   end
 end
--- test/lib/services/reports/types/recruiter_survey/statistic.exs
+++ test/lib/services/reports/types/recruiter_survey/statistic.exs
@@ -5,41 +5,63 @@ defmodule KlziiChat.Services.Reports.Types.RecruiterSurvey.StatisticTest do
   describe "#ap_answers" do
     test "build answer data structur" do
       answers = [
-        %{"10" => %{},
-        "6" => %{"type" => "number", "value" => 1},
-        "7" => %{"type" => "number", "value" => 0},
-        "8" => %{"type" => "string", "value" => "xfcb"},
-        "9" => %{"type" => "number", "value" => 1}
-        },
-        %{"10" => %{"contactDetails" => %{
-          "age" => "18-19",
-          "email" => "pff@gmail.com",
-          "firstName" => "asss",
-          "gender" => "male",
-          "lastName" => "ssss",
-          "mobile" => "+61334344564"
-          },
-          "tagHandled" => true,
-          "type" => "object",
-          "value" => nil
-          },
-          "6" => %{"type" => "number", "value" => 0},
+        %{
+          "10" => %{},
+          "6" => %{"type" => "number", "value" => 1},
           "7" => %{"type" => "number", "value" => 0},
           "8" => %{"type" => "string", "value" => "xfcb"},
+          "9" => %{"type" => "number", "value" => 1}
+        },
+        %{
+          "10" => %{
+            "contactDetails" => %{
+              "age" => "18-19",
+              "email" => "pff@gmail.com",
+              "firstName" => "asss",
+              "gender" => "male",
+              "lastName" => "ssss",
+              "mobile" => "+61334344564"
+            },
+            "tagHandled" => true,
+            "type" => "object",
+            "value" => nil
+          },
+          "6" => %{"type" => "number", "value" => 1},
+          "7" => %{"type" => "number", "value" => 0},
+          "8" => %{"type" => "string", "value" => "xfcb"},
+          "9" => %{"type" => "number", "value" => 0}
+        },
+        %{
+          "10" => %{
+            "contactDetails" => %{
+              "age" => "50-54",
+              "email" => "pff@gmail.com",
+              "firstName" => "asss",
+              "gender" => "female",
+              "lastName" => "ssss",
+              "mobile" => "+61334344564"
+            },
+            "tagHandled" => true,
+            "type" => "object",
+            "value" => nil
+          },
+          "6" => %{"type" => "number", "value" => 5},
+          "7" => %{"type" => "number", "value" => 3},
+          "8" => %{"type" => "string", "value" => "pff"},
           "9" => %{"type" => "number", "value" => 0}
         }
       ]
 
-          expect_result = %{
-            "10" => %{count: 1,type: "object", values: %{"18-19" => 1}},
-            "9" => %{count: 2, type: "number", values: %{0 => 1, 1 => 1}},
-            "8" => %{count: 2, type: "string",  values: ["xfcb", "xfcb"]},
-            "7" => %{count: 2, type: "number", values: %{0 => 1, 1 => 1}},
-            "6" => %{count: 2, type: "number", values: %{0 => 1, 1 => 1}},
-          }
+      expect_result = %{
+        "10" => %{count: 3,type: "object", values: %{"age" => %{"18-19" => 1, "50-54" => 1}, "gender" => %{"male" => 1, "female" => 1}}},
+        "9" => %{count: 3, type: "number", values: %{0 => 2, 1 => 1}},
+        "8" => %{count: 3, type: "string",  values: ["xfcb", "xfcb", "pff"]},
+        "7" => %{count: 3, type: "number", values: %{0 => 2, 3 => 1}},
+        "6" => %{count: 3, type: "number", values: %{1 => 2, 5 => 1}},
+      }
 
-          resp = Statistic.map_answers(answers)
-          assert(expect_result == resp)
+      resp = Statistic.map_answers(answers)
+      assert(expect_result == resp)
 
     end
   end
