4b66872c78cef81ff5312a5d36961f42eb7a0973
DainisL <dainis186@gmail.com>
2017-02-06 17:24:38 +0200

    split textarea qestions to other sheets

--- lib/klzii_chat/services/reports/types/recruiter_survey/formats/xlsx.ex
+++ lib/klzii_chat/services/reports/types/recruiter_survey/formats/xlsx.ex
@@ -1,5 +1,4 @@
 defmodule KlziiChat.Services.Reports.Types.RecruiterSurvey.Formats.Xlsx do
-  alias KlziiChat.Services.Reports.Types.RecruiterSurvey.DataContainer
   alias Elixlsx.{Sheet, Workbook}
   @spec processe_data(Map.t) :: {String.t}
   def processe_data(data) do
@@ -13,22 +12,45 @@ defmodule KlziiChat.Services.Reports.Types.RecruiterSurvey.Formats.Xlsx do
     header_sheet = Sheet.with_name(get_in(data, ["header_title"]))
             |> Sheet.set_cell("A1", "Recruiter Stats Report")
             |> Sheet.set_cell("A3", get_in(data, ["header_title"]))
-            |> Sheet.set_cell("C3", "Count")
-            |> Sheet.set_cell("D3", "%")
             |> Sheet.set_cell("A4", "Total Surveys Completed")
-            |> Sheet.set_cell("E4", get_in(stats, [:total_count]))
-    sheet_acc = %{sheet: header_sheet, line_number: 5}
+            |> Sheet.set_cell("C3", "Count")
+            |> Sheet.set_cell("C4", get_in(stats, [:total_count]))
+            |> Sheet.set_cell("D3", "%")
+            |> Sheet.set_cell("D4", get_in(stats, [:total_count_percents]) || 100)
+    sheet_acc = %{main_sheet: header_sheet, line_number: 5, sheets: [] }
 
-    %{sheet: sheet} =
+    %{main_sheet: main_sheet, sheets: sheets, line_number: _} =
       Enum.reduce(get_in(stats, [:questions]), sheet_acc, fn(question, acc) ->
-        tmp_sheet =
-          Sheet.set_cell(acc.sheet, "A#{acc.line_number}", get_in(question, [:name]))
-        Map.put(acc, :sheet, tmp_sheet)
-          |> map_data_answer(question.answers)
+        map_question(acc, question)
      end)
 
-    workbook = %Workbook{sheets: [sheet]}
+    workbook = %Workbook{sheets: [main_sheet] ++ sheets }
     {:ok, %{data: workbook, header: []}}
+  end
+
+  def map_question(acc, %{type: "textarea", name: name} = question) do
+    tmp_sheet =
+      Sheet.with_name(name)
+      |> Sheet.set_cell("A1", name)
+
+      valuses =
+        question.answers
+        |> List.first
+        |> Map.get(:values)
+
+      %{sheet: sheet} =
+        Enum.reduce(valuses, %{sheet: tmp_sheet, line_number: 2}, fn(answer, sheet_acc) ->
+          t_sheet = Sheet.set_cell(sheet_acc.sheet, "A#{sheet_acc.line_number}", answer)
+          Map.put(sheet_acc, :sheet,  t_sheet)
+          |> Map.put(:line_number, sheet_acc.line_number + 1)
+        end)
+    Map.put(acc, :sheets, acc.sheets ++ [sheet])
+  end
+  def map_question(acc, question) do
+    tmp_sheet =
+      Sheet.set_cell(acc.main_sheet, "A#{acc.line_number}", get_in(question, [:name]))
+    Map.put(acc, :main_sheet, tmp_sheet)
+      |> map_data_answer(question.answers)
   end
 
   @spec map_data_answer(Map.t,  List.t) :: Map.t
@@ -36,14 +58,13 @@ defmodule KlziiChat.Services.Reports.Types.RecruiterSurvey.Formats.Xlsx do
     map_data_answer(acc, head)
     |> map_data_answer(tail)
   end
-
   def map_data_answer(acc, answer) do
     tmp_sheet =
-      Sheet.set_cell(acc.sheet, "B#{acc.line_number}", get_in(answer, [:name]))
+      Sheet.set_cell(acc.main_sheet, "B#{acc.line_number}", get_in(answer, [:name]))
       |> Sheet.set_cell("C#{acc.line_number}", get_in(answer, [:count]))
       |> Sheet.set_cell("D#{acc.line_number}", get_in(answer, [:percents]))
 
-    Map.put(acc, :sheet, tmp_sheet)
+    Map.put(acc, :main_sheet, tmp_sheet)
     |> Map.put(:line_number, acc.line_number + 1)
   end
 end
