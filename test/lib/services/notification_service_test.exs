ee0bab3b839a9848bac902be0d84c7a891c6c403
Nazar Perepichka <nazar.perepichka@diatomenterprises.com>
2017-02-08 09:18:12 +0200

    tests

--- test/lib/services/notification_service_test.exs
+++ test/lib/services/notification_service_test.exs
@@ -1,17 +1,22 @@
 defmodule KlziiChat.Services.NotificationServiceTest do
   use KlziiChat.{ModelCase, SessionMemberCase}
+  alias KlziiChat.Services.{NotificationService}
 
   setup %{session: session, facilitator: facilitator, participant: participant} do
-    {:ok, participant: participant, parent_message: parent_message, session_name: session.name}
+    {:ok, participant: participant, facilitator: facilitator, session_id: session.id}
   end
 
   describe "send notification" do
-    test "if online", %{session_name: session_name, participant: participant} do
-      #todo: 
+    test "if online", %{session_id: session_id, participant: participant} do
+      active_users = %{ to_string(participant.id) => %{} }
+      res = NotificationService.send_notification_if_need(participant.id, active_users, session_id, "private_message")
+      assert({:ok, false} = res)
     end
 
-    test "if offline", %{session_name: session_name, participant: participant} do
-      #todo: 
+    test "if offline", %{session_id: session_id, participant: participant} do
+      active_users = %{ }
+      res = NotificationService.send_notification_if_need(participant.id, active_users, session_id, "chat_message")
+      assert({:ok, true} = res)
     end
   end
 
