37f3598a55b9487aec6b8279c555b9905b11577d
warlockxins <warlockxins@gmail.com>
2017-02-09 23:06:14 +0200

    scale functionality for whiteboard

--- web/static/js/components/whiteboard/toolbar/zoom.js
+++ web/static/js/components/whiteboard/toolbar/zoom.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { OverlayTrigger, Button } from 'react-bootstrap';
+import { OverlayTrigger, Button, Popover } from 'react-bootstrap';
 import mobileScreenHelpers from '../../../mixins/mobileHelpers';
 
 const ButtonZoom = React.createClass({
@@ -9,10 +9,31 @@ const ButtonZoom = React.createClass({
   onClick() {
     this.props.setType(this.state.buttonType);
   },
+  onZoomClick(zoomIn) {
+    this.props.zoomIn(zoomIn);
+  },
+  onZoomRestore() {
+    this.props.zoomRestore();
+  },
   render() {
-    if (mobileScreenHelpers.isMobile()) {
+   if (mobileScreenHelpers.isMobile()) {
       return (
-        <OverlayTrigger placement='top' overlay={ this.props.tooltipFormat('Zoom') }>
+        <OverlayTrigger ref='zoom' trigger='click' rootClose placement='top' overlay={
+            <Popover id='wb-buttons-delete'>
+              <OverlayTrigger placement='top' overlay={ this.props.tooltipFormat('Zoom out') }>
+                <i className='btn btn-default fa fa-search-minus' aria-hidden='true' onClick={ this.onZoomClick.bind(this, false) } />
+              </OverlayTrigger>
+
+              <OverlayTrigger placement='top' overlay={ this.props.tooltipFormat('Zoom in') }>
+                <i className='btn btn-default fa fa-search-plus' aria-hidden='true' onClick={ this.onZoomClick.bind(this, true) } />
+              </OverlayTrigger>
+
+              <OverlayTrigger placement='top' overlay={ this.props.tooltipFormat('Reset zoom') }>
+                <i className='btn btn-default fa fa-arrows-alt' aria-hidden='true' onClick={ this.onZoomRestore.bind(this) } />
+              </OverlayTrigger>
+            </Popover>
+          }>
+
           <Button className={ this.props.getClassnameParent(this.state.buttonType) + 'btn btn-default' } onClick={ this.onClick }>
             <i className='fa fa-search-plus' aria-hidden='true' />
           </Button>
