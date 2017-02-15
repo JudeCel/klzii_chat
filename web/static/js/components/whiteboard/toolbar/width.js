34b8488399b9b8c2540b166ac11d93fe98d68f5d
Alexander Semionov <alexander.semionov@diatomenterprises.com>
2017-02-15 15:37:03 +0200

    update width selection for lines

--- web/static/js/components/whiteboard/toolbar/width.js
+++ web/static/js/components/whiteboard/toolbar/width.js
@@ -1,20 +1,28 @@
 import React from 'react';
-import { DropdownButton, MenuItem, OverlayTrigger } from 'react-bootstrap';
+import { DropdownButton, MenuItem, OverlayTrigger, Popover, Button } from 'react-bootstrap';
 
 const ButtonsWidth = React.createClass({
+  setWidth(width) {
+    this.props.setWidth(width);
+  },
   render() {
     const widths = [6, 4, 2];
     const { strokeWidth } = this.props;
-
     return (
-      <OverlayTrigger placement='top' overlay={ this.props.tooltipFormat('Size') }>
-        <DropdownButton title={ strokeWidth } dropup noCaret className='fa fa-cog' id='wb-buttons-width'>
-          {
-            widths.map((value) =>
-              <MenuItem key={ value } eventKey={ value } active={ strokeWidth == value } onSelect={ this.props.setWidth } >{ value }</MenuItem>
-            )
-          }
-        </DropdownButton>
+      <OverlayTrigger ref='zoom' trigger='click' rootClose placement='top' overlay={
+          <Popover id='wb-buttons-width'>
+            {
+              widths.map((value) =>
+                  <OverlayTrigger key={value} placement='top' overlay={ this.props.tooltipFormat(value) }>
+                    <div className="btn btn-default" onClick={ this.setWidth.bind(this, value) }>{value}</div>
+                  </OverlayTrigger>
+              )
+            }
+          </Popover>
+        }>
+        <Button className={ 'btn btn-default' }>
+          {strokeWidth} <i className='fa fa fa-cog' aria-hidden='true' />
+        </Button>
       </OverlayTrigger>
     )
   }
