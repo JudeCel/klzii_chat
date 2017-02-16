b138d0d76f23a6eaa27cbf684d2bba76bab6519a
DainisL <dainis186@gmail.com>
2017-02-16 11:09:08 +0200

    update readme

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule KlziiChat.Mixfile do
   def project do
     [app: :klzii_chat,
      version: "0.0.1",
-     elixir: "~> 1.3",
+     elixir: "~> 1.4",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
