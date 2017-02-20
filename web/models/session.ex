62554434bd8fdbbcec1d110f9eb3b50f85e89eff
Nazar Perepichka <nazar.perepichka@diatomenterprises.com>
2017-02-20 16:20:17 +0200

    Models changed

--- web/models/session.ex
+++ web/models/session.ex
@@ -2,14 +2,13 @@ defmodule KlziiChat.Session do
   use KlziiChat.Web, :model
 
   @moduledoc """
-    This Mode is read only!
+    This Model is read only!
     Not use for insert!
   """
 
   schema "Sessions" do
     field :name, :string
     field :status, :string
-    field :type, :string
     field :timeZone, :string
     field :brand_project_id, :integer
     field :startTime, Timex.Ecto.DateTime
@@ -26,6 +25,7 @@ defmodule KlziiChat.Session do
     has_many :session_topics, KlziiChat.SessionTopic, [foreign_key: :sessionId]
     has_many :topics, through: [:session_topics, :topic]
     has_many :direct_messages, KlziiChat.DirectMessage, [foreign_key: :sessionId]
+    belongs_to :session_type, KlziiChat.SessionType, [foreign_key: :type, references: :name, type: :string]
     timestamps [inserted_at: :createdAt, updated_at: :updatedAt]
   end
 end
