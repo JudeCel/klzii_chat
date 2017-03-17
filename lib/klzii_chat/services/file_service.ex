901bb17f63affd20e0d3f2e8d35093be9ea67c06
warlockxins <alexander.semionov@diatomenterprises.com>
2017-03-17 17:44:26 +0200

    change back pdf file config

--- lib/klzii_chat/services/file_service.ex
+++ lib/klzii_chat/services/file_service.ex
@@ -55,13 +55,13 @@ defmodule KlziiChat.Services.FileService do
 
   def conwert_with_xvfb(tmp_body, tmp_header, destination_path) do
     options = [
-      #{}"--auto-servernum",
-      #{}"wkhtmltopdf",
+      "--auto-servernum",
+      "wkhtmltopdf",
       "--page-size", "A4", "--dpi", "100",
       "--header-spacing", "5", "--window-status", "js-done",
       "--footer-html", @footer_path, "--header-html", tmp_header, "file://" <> tmp_body, destination_path
     ]
-    case System.cmd("wkhtmltopdf", options , stderr_to_stdout: true) do
+    case System.cmd("xvfb-run", options , stderr_to_stdout: true) do
       {_, 0} ->
         :ok = File.rm(tmp_header)
         :ok = File.rm(tmp_body)
