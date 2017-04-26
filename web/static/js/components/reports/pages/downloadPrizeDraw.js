6d0ac46d0045a872aca00a45223dbc5f5ed2b435
warlockxins <alexander.semionov@diatomenterprises.com>
2017-04-26 17:51:22 +0300

    spelling fix

--- web/static/js/components/reports/pages/downloadPrizeDraw.js
+++ web/static/js/components/reports/pages/downloadPrizeDraw.js
@@ -8,7 +8,7 @@ const DownloadPrizeDrawReport = React.createClass({
       <div className='download-section'>
         <div className='col-md-12 text-center dialog-text-mod'>
           <h3><b>Please Note!</b></h3>
-          <p><h4>This CSV is to be used for the purposes of a Prize Draw only. It may contain details of Guests who do not want to be cantacted again. Except to be notified about the Prize Draw.</h4></p>
+          <p><h4>This CSV is to be used for the purposes of a Prize Draw only. It may contain details of Guests who do not want to be contacted again. Except to be notified about the Prize Draw.</h4></p>
           <p><i>FYI, those to Invite Again are in a New Contact List.</i></p>
         </div>
 
