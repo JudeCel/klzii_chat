25abf75a8448fef8ffd58525aedd064074335268
Sergey Ryabkov <s.ryabkov@aimprosoft.com>
2017-11-13 18:22:37 +0200

    TA1906: - if user clicks the back arrow but it will tell user that he is about to leave the website

--- web/static/js/components/header/mobile.js
+++ web/static/js/components/header/mobile.js
@@ -9,6 +9,10 @@ import MobileConsole from '../../components/console/mobileConsole.js';
 
 const MobileHeader = React.createClass({
   mixins: [mixins.modalWindows, mixins.validations, mixins.headerActions],
+  componentDidMount() {
+    // TA1906 - if you want to go back users are clicking the back arrow but it takes them out of the Session and back to the login
+    window.onbeforeunload = function() { return ""; };
+  },
   getInitialState() {
     return { mobileSideMenuVisibility: false, mobileSideMenuTopicsVisibility: false };
   },
