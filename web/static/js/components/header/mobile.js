e1322c4e88e40f028e973cb6f59398c2b65c06fe
Sergey Ryabkov <s.ryabkov@aimprosoft.com>
2017-12-13 13:50:58 +0200

    TA1981: - try to handle back button on mobile using pushState&onpopstate

--- web/static/js/components/header/mobile.js
+++ web/static/js/components/header/mobile.js
@@ -1,4 +1,3 @@
-
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import mixins from '../../mixins';
@@ -11,7 +10,16 @@ const MobileHeader = React.createClass({
   mixins: [mixins.modalWindows, mixins.validations, mixins.headerActions],
   componentDidMount() {
     // TA1906 - if you want to go back users are clicking the back arrow but it takes them out of the Session and back to the login
-    window.onbeforeunload = function() { return ""; };
+    window.onload = function () {
+      history.pushState(Math.random(), null, null);
+      window.onpopstate = function () {
+        if (confirm('You are about leaving the page. Do you want to leave it?')) {
+          history.back();
+        } else {
+          history.pushState(Math.random(), null, null);
+        }
+      };
+    };
   },
   getInitialState() {
     return { mobileSideMenuVisibility: false, mobileSideMenuTopicsVisibility: false };
